3c996ee4c6848aabc6e02bd51cc953d594afacd1
abenezer121 <webgoat12@gmail.com>
2022-10-24 18:17:11 +0300

    push usage added

--- src/components/New/ProfilePopup.js
+++ src/components/New/ProfilePopup.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux"
+
 import { setUser } from "./../../redux/reducers/user";
 import {setPlan} from "./../../redux/reducers/plan"
 import {setChoosedPlan} from "./../../redux/reducers/choosedplans"
+import { useSelector, useDispatch } from "react-redux"
 function ProfilePopup() {
   const dispatch = useDispatch()
+  const { userData } = useSelector((state) => state.user)
   return (
     <div className="flex flex-col ">
       <div className="flex justify-end px-4">
@@ -13,11 +15,11 @@ function ProfilePopup() {
       </div>
       <div className="border border-white rounded-md p-4 px-6 bg-[#1A1F32]">
         <div className="flex flex-col gap-2">
-          <Link to="/" className="text-white">Profile Settings</Link>
+          <Link to="/account/settings" className="text-white">Profile Settings</Link>
           <Link to="/" className="text-white">Contact Support</Link>
-          <Link to="/" className="text-white">Usage</Link>
+          <Link to="/account/usage" className="text-white">Usage</Link>
           <hr className="border-gray-500" />
-          <span className="text-gray-500">username</span>
+          <span className="text-gray-500">{userData.username}</span>
             
           <Link to="/"  onClick = {()=>{
                 dispatch(setUser({}))
