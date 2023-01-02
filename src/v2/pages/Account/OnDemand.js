53b960c73924decc70779f680b9e0c0cc1c6020b
Yeabsira Abebe <abebey348@gmail.com>
2023-01-02 10:49:10 +0300

    update on demand and notification

--- src/v2/pages/Account/OnDemand.js
+++ src/v2/pages/Account/OnDemand.js
@@ -24,6 +24,24 @@ function Cards() {
           </div>
         </div>
         <div className="gap-4  flex-wrap flex-1 min-w-[200px] text-[#777] flex ">
+          <div className="leading-3 flex-1 flex gap-3 p-4 rounded-xl bg-[#202022] ">
+            <div className="">
+              <h2 className="p-0 m-0 uppercase ">Upload File</h2>
+              <p className="m-0 p-0">CSV</p>
+            </div>
+            <div className="flex flex-1 items-stretch">
+              <input type="text" placeholder="enter path" className="flex-1 bg-[#181818] px-3 border-0" />
+              <input type='submit' value='Browse' className="btn_sty1  font-bold !bg-btnprimary/40 !text-btnprimary !border-btnprimary/10" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    {/* Manual Entry */}
+    <div className="flex gap-4 items-start">
+      <div className="flex flex-col flex-1 gap-4">
+        <div className="flex gap-4">
           <div className="leading-3 flex-1 flex gap-3 p-4 rounded-xl bg-[#202022]">
             <div className="">
               <h2 className="p-0 m-0 uppercase ">Manual</h2>
@@ -46,20 +64,62 @@ function Cards() {
             </div>
           </div>
 
+          <input type='submit' value='+' className="btn_sty1 !font-extrabold !text-2xl !px-6 rounded-md !bg-btnprimary/40 !text-btnprimary !border-btnprimary/10" />
+        </div>
+
+        <div className="flex gap-4">
+          <div className="leading-3 flex-1 flex gap-3 p-4 rounded-xl bg-[#202022]">
+            <div className="">
+              <h2 className="p-0 m-0 uppercase ">Manual</h2>
+              <p className="m-0 p-0">entry</p>
+            </div>
+            <div className="flex flex-1 items-stretch">
+              <input type="text" placeholder="enter location name" className="flex-1 bg-[#181818] px-3 border-0" />
+              <div className="flex items-end p-2">Lat</div>
+            </div>
+          </div>
+
+          <div className="leading-3 flex-1 flex gap-3 p-4 rounded-xl bg-[#202022]">
+            <div className="">
+              <h2 className="p-0 m-0 uppercase ">Manual</h2>
+              <p className="m-0 p-0">entry</p>
+            </div>
+            <div className="flex flex-1 items-stretch">
+              <input type="text" placeholder="9.34234221" className="flex-1 bg-[#181818] px-3 border-0" />
+              <div className="flex items-end p-2">Long</div>
+            </div>
+          </div>
+
+          <input type='submit' value='+' className="btn_sty1 !font-extrabold !text-2xl !px-6 rounded-md !bg-btnprimary/40 !text-btnprimary !border-btnprimary/10" />
+
+        </div>
+
+        <div className="flex gap-4">
+          <div className="leading-3 flex-1 flex gap-3 p-4 rounded-xl bg-[#202022]">
+            <div className="">
+              <h2 className="p-0 m-0 uppercase ">Manual</h2>
+              <p className="m-0 p-0">entry</p>
+            </div>
+            <div className="flex flex-1 items-stretch">
+              <input type="text" placeholder="enter location name" className="flex-1 bg-[#181818] px-3 border-0" />
+              <div className="flex items-end p-2">Lat</div>
+            </div>
+          </div>
+
+          <div className="leading-3 flex-1 flex gap-3 p-4 rounded-xl bg-[#202022]">
+            <div className="">
+              <h2 className="p-0 m-0 uppercase ">Manual</h2>
+              <p className="m-0 p-0">entry</p>
+            </div>
+            <div className="flex flex-1 items-stretch">
+              <input type="text" placeholder="9.34234221" className="flex-1 bg-[#181818] px-3 border-0" />
+              <div className="flex items-end p-2">Long</div>
+            </div>
+          </div>
+          <input type='submit' value='+' className="btn_sty1 !font-extrabold !text-2xl !px-6 rounded-md !bg-btnprimary/40 !text-btnprimary !border-btnprimary/10" />
         </div>
       </div>
-    </div>
-    <div className="flex gap-4 ">
-      <div className="leading-3 flex-1 flex gap-3 p-4 rounded-xl bg-[#202022] ">
-        <div className="">
-          <h2 className="p-0 m-0 uppercase ">Upload File</h2>
-          <p className="m-0 p-0">CSV</p>
-        </div>
-        <div className="flex flex-1 items-stretch">
-          <input type="text" placeholder="enter path" className="flex-1 bg-[#181818] px-3 border-0" />
-          <input type='submit' value='Browse' className="btn_sty1  font-bold !bg-btnprimary/40 !text-btnprimary !border-btnprimary/10" />
-        </div>
-      </div>
+
       <div className="leading-3 flex gap-3 p-4 rounded-xl bg-[#202022] ">
         <div className="">
           <h2 className="p-0 m-0 uppercase ">Driver</h2>
